cef6f0724e6610c3ba3434ec9560a1f8fca5ca2d
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-09-19 08:38:08 -0300

    Primeiro commit, programa feito e Tester criado

---
+++ EstacionamentoConsole/Models/TipoVeiculo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionamentoConsole
+{
+    public enum TipoVeiculo
+    {
+        Automovel = 0,
+        Motocicleta = 1,
+    }
+}
