97994cffd96a66a1185befe6967410c3ff2422c3
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-09-25 11:15:35 -0300

    Aplicando teste em na classe Patio

---
+++ Estacionamento.Testes/PatioTestes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EstacionamentoConsole;
+using EstacionamentoConsole.Models;
+
+namespace Estacionamento.Testes
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            //Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo();
+            veiculo.Proprietario = "José Edson Noé";
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = "Verde";
+            veiculo.Modelo = "Fusca";
+            veiculo.Placa = "BCB-9999";
+
+            
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+
+        }
+    }
+}
--- Estacionamento.Testes/VeiculoTestes.cs
+++ Estacionamento.Testes/VeiculoTestes.cs
@@ -1,3 +1,4 @@
+using EstacionamentoConsole;
 using EstacionamentoConsole.Models;
 using NuGet.Frameworks;
 
@@ -5,11 +6,17 @@ namespace Estacionamento.Testes
 {
     public class VeiculoTestes
     {
+        
+        //Padrão A.A.A
+        
         [Fact]
         public void TestaVeiculoAcelerar()
         {
+            //Arrange
             var veiculo = new Veiculo();
+            //Act
             veiculo.Acelerar(10);
+            //Assert 
             Assert.Equal(100, veiculo.VelocidadeAtual);
 
 
@@ -18,9 +25,22 @@ namespace Estacionamento.Testes
         [Fact]
         public void TestaVeiculoFrear()
         {
+            //Arrange
             var veiculo = new Veiculo();
+            //Act
             veiculo.Frear(10);
+            //Assert 
             Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaTipoVeiculo()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+            //Act
+            //Assert
+            Assert.Equal(TipoVeiculo.Automovel, veiculo.Tipo);
         }
     }
 }
