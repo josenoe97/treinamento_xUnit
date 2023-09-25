ac497f3b76241dc8749a943f42dfa90c35124ee7
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-09-25 11:38:57 -0300

    Reutilizacao de código com as notações Theory e InlineData

--- Estacionamento.Testes/PatioTestes.cs
+++ Estacionamento.Testes/PatioTestes.cs
@@ -33,5 +33,31 @@ namespace Estacionamento.Testes
             Assert.Equal(2, faturamento);
 
         }
+
+        [Theory]
+        [InlineData("André Silva", "ASD-1498", "preto", "Gol" )]
+        [InlineData("José Silva", "POL-1298", "branco", "Etios")]
+        [InlineData("Carlos Noe", "ADF-1423", "azul", "Opala")]
+
+        public void ValidaFaturamentoComVariosVeiculos(string proprietario,
+                                                       string placa,
+                                                       string cor,
+                                                       string modelo)
+        {
+            //Arrange
+            Patio estacionamento = new Patio();
+            var veiculo = new Veiculo();
+            veiculo.Proprietario = proprietario;
+            veiculo.Placa = placa;
+            veiculo.Cor = cor;
+            veiculo.Modelo = modelo;
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+            //Act
+            double faturamento = estacionamento.TotalFaturado();
+            //Assert
+            Assert.Equal(2, faturamento);
+            
+        }
     }
 }
